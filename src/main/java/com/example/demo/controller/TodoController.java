a28386a29be48b2d7665a2cac409f45550bf5ff7
haohao6683 <852721803@qq.com>
2020-08-06 22:09:31 +0800

    [todo-list][Draymond] Implement: Implement the method should_return_todo_list_when_find_all_todo_list_given_nothing.

---
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Todo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.service.TodoService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Todo> getTodoList() {
+        return todoService.getTodoList();
+    }
+}
--- src/test/java/com/example/demo/service/TodoListIntegrationTest.java
+++ src/test/java/com/example/demo/service/TodoListIntegrationTest.java
@@ -1,20 +1,20 @@
-package service;
+package com.example.demo.service;
 
-import model.Todo;
+import com.example.demo.model.Todo;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import repository.TodoRepository;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import com.example.demo.repository.TodoRepository;
 import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TodoListIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
@@ -22,20 +22,22 @@ public class TodoListIntegrationTest {
     @Autowired
     private TodoRepository todoRepository;
 
+    @AfterEach
+    void tearDown() {
+        todoRepository.deleteAll();
+    }
+
     @Test
     void should_return_todo_list_when_find_all_todo_list_given_nothing() throws Exception {
         //given
-        List<Todo> mockedTodos = new ArrayList<>();
-        mockedTodos.add(new Todo(1, "todo-1", true));
-        mockedTodos.add(new Todo(1, "todo-1", true));
-        when(todoRepository.findAll()).thenReturn(mockedTodos);
+        Todo savedTodo = todoRepository.save(new Todo(1, "todo-1", true));
 
         //when then
         mockMvc.perform(get("/todos"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].id").value(mockedTodos.get(0).getId()))
-                .andExpect(jsonPath("$[0].text").value(mockedTodos.get(0).getText()))
-                .andExpect(jsonPath("$[0].status").value(mockedTodos.get(0).isStatus()));
+                .andExpect(jsonPath("$[0].id").value(savedTodo.getId()))
+                .andExpect(jsonPath("$[0].text").value(savedTodo.getText()))
+                .andExpect(jsonPath("$[0].status").value(savedTodo.isStatus()));
     }
 }
