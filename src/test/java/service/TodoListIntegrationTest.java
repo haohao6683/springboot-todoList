4cd2f9a43af689537e665bbfa4dc873132e659c0
haohao6683 <852721803@qq.com>
2020-08-06 11:41:59 +0800

    [todo-list][draymond] refactor: refactor the test method should_return_todo_list_when_find_all_todo_list_given_nothing.

--- src/test/java/service/TodoListIntegrationTest.java
+++ src/test/java/service/TodoListIntegrationTest.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 import repository.TodoRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,16 +25,17 @@ public class TodoListIntegrationTest {
     @Test
     void should_return_todo_list_when_find_all_todo_list_given_nothing() throws Exception {
         //given
-        Todo todo = new Todo(1, "todo-1", true);
-        Todo save = todoRepository.save(todo);
+        List<Todo> mockedTodos = new ArrayList<>();
+        mockedTodos.add(new Todo(1, "todo-1", true));
+        mockedTodos.add(new Todo(1, "todo-1", true));
+        when(todoRepository.findAll()).thenReturn(mockedTodos);
 
         //when then
         mockMvc.perform(get("/todos"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].id").value(save.getId()))
-                .andExpect(jsonPath("$[0].text").value(save.getText()))
-                .andExpect(jsonPath("$[0].status").value(save.isStatus()));
-
+                .andExpect(jsonPath("$[0].id").value(mockedTodos.get(0).getId()))
+                .andExpect(jsonPath("$[0].text").value(mockedTodos.get(0).getText()))
+                .andExpect(jsonPath("$[0].status").value(mockedTodos.get(0).isStatus()));
     }
 }
